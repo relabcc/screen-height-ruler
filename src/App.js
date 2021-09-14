1e3376f86dcfa0c7b613733497c4133d404f7525
Renddi Yeh <renddi@relab.cc>
2021-09-14 17:46:17 +0800

    added vh

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import './App.css';
 function App() {
   const [size, setSize] = useState()
   const [x2, setX2] = useState()
+  const [vh, setVh] = useState()
   return (
     <Measure onResize={({ bounds }) => setSize(bounds)} bounds>
       {({ measureRef }) => (
-        <div ref={measureRef} style={{ height: `${x2 ? 2 : 1}00%`, overflow: 'hidden', backgroundColor: 'beige' }}>
+        <div ref={measureRef} style={{ height: `${x2 ? 2 : 1}00${vh ? 'vh' : '%'}`, overflow: 'hidden', backgroundColor: 'beige' }}>
           {size && size.height && (
             <>
               {range(0, Math.floor(size.height), 50).map(d => (
@@ -21,6 +22,10 @@ function App() {
                 <label>
                   <input type="checkbox" name="x2" onChange={(e) => setX2(e.target.checked)} checked={x2} />
                   2x height
+                </label>
+                <label>
+                  <input type="checkbox" name="vh" onChange={(e) => setVh(e.target.checked)} checked={vh} />
+                  use vh
                 </label>
                 {Math.floor(size.width)}px x {Math.floor(size.height)}px
               </div>
